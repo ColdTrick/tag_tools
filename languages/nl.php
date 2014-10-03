297431229e9d1cce1db6c9afcfd2b99d79416dcd
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-10-03 10:08:58 +0200

    updated dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,16 +1,23 @@
 <?php
-return array(
-	'tag_tools:notifications:menu' => "Tag notificaties",
-	'tag_tools:notifications:description' => "Beheer de tags die je in de gaten houd en stel in of en hoe je van nieuwe content met deze tag op de hoogte wilt worden gehouden.",
-	'tag_tools:notifications:empty' => "Op dit moment volg je geen tags. Indien je een tag ziet, kun je deze volgen middels het icoon naast de tag.",
-	'tag_tools:notification:follow:subject' => "Nieuwe content met de tag %s",
-	'tag_tools:notification:follow:message' => "Hallo, 
+/**
+ * This file was created by Translation Editor v2.0
+ * On 2014-10-03 10:08
+ */
+
+return array (
+  'tag_tools:follow_tag:menu:on' => 'Volg nieuwe content met deze tag',
+  'tag_tools:follow_tag:menu:off' => 'Stop met volgen van nieuwe content met deze tag',
+  'tag_tools:notifications:menu' => 'Tag notificaties',
+  'tag_tools:notifications:description' => 'Beheer de tags die je in de gaten houd en stel in of en hoe je van nieuwe content met deze tag op de hoogte wilt worden gehouden.',
+  'tag_tools:notifications:empty' => 'Op dit moment volg je geen tags. Indien je een tag ziet, kun je deze volgen middels het icoon naast de tag.',
+  'tag_tools:notification:follow:subject' => 'Nieuwe content met de tag %s',
+  'tag_tools:notification:follow:message' => 'Hallo, 
 
 er is nieuwe content met de tag %s. 
 
-Bekijk het hier: %s",
-	'tag_tools:activity:tags' => "Activiteit gebaseerd op tags die je volgt",
-	'tag_tools:widgets:follow_tags:title' => "Gevolgde tags",
-	'tag_tools:widgets:follow_tags:description' => "Toont de tags die je volgt",
-	'tag_tools:widgets:follow_tags:empty' => "Deze gebruiker volgt op dit moment geen tags",	
+Bekijk het hier: %s',
+  'tag_tools:activity:tags' => 'Activiteit gebaseerd op tags die je volgt',
+  'tag_tools:widgets:follow_tags:title' => 'Gevolgde tags',
+  'tag_tools:widgets:follow_tags:description' => 'Toont de tags die je volgt',
+  'tag_tools:widgets:follow_tags:empty' => 'Deze gebruiker volgt op dit moment geen tags',
 );
