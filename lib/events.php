32226892f6d66bcf1be669ab3b7a0e76274d27fb
Jerome Bakker <jeabakker@coldtrick.com>
2014-11-06 16:25:34 +0100

    fix link to content instead of activity stream fix bugs

--- lib/events.php
+++ lib/events.php
@@ -4,45 +4,68 @@
  */
 
 function tag_tools_create_metadata_event_handler($event, $type, ElggMetadata $metadata) {
+	
+	// is it a tag
+	if ($metadata->name != "tags") {
+		return;
+	}
+	
+	// get the entity for further use
 	$ia = elgg_set_ignore_access(true);
 	
 	$entity_guid = $metadata->entity_guid;
 	// can't use elgg get entity because cache is not correctly updated
-	$entity = get_entity_as_row($entity_guid);
+	$entity_row = get_entity_as_row($entity_guid);
 	
+	elgg_set_ignore_access($ia);
+	
+	// shortcut for private entities
+	if ($entity_row->access_id == ACCESS_PRIVATE) {
+		return;
+	}
+	
+	// only send notifications on creation of the entity
 	$time_created_treshold = 5;
-	
-	if ($entity->time_created < (time() - 5)) {
+	if ($entity_row->time_created < (time() - 5)) {
 		// assume it is an update
 		return;
 	}
 	
-	if ($metadata->name == "tags") {
-		$tag = $metadata->value;
+	$tag = $metadata->value;
+	
+	$options = array(
+		"type" => "user",
+		"annotation_name_value_pairs" => array(
+			"name" => "follow_tag",
+			"value" => $tag
+		),
+		"limit" => false
+	);
+
+	$ia = elgg_set_ignore_access(true);
+	
+	$dbprefix = elgg_get_config("dbprefix");
+	$entities = new ElggBatch("elgg_get_entities_from_annotations", $options);
+	foreach ($entities as $user) {
 		
-		$options = array(
-			'type' => "user",
-			'annotation_name' => "follow_tag",
-			'annotation_value' => $tag,
-			'limit' => false
-		);		
+		// check if not trying to notify the owner
+		if ($user->getGUID() == $entity_row->owner_guid) {
+			continue;
+		}
 		
-		$entities = elgg_get_entities_from_annotations($options);
+		// check access for the user, can't use has_access_to_entity
+		// because that requires a full entity
+		$access_bit = _elgg_get_access_where_sql(array("user_guid" => $user->getGUID()));
 		
-		$dbprefix = elgg_get_config("dbprefix");
-		foreach ($entities as $user) {
-			
-			// check access, can't use has_access_to_entity
-			$access_bit = _elgg_get_access_where_sql(array('user_guid' => $user->getGUID()));
-			
-			$query = "SELECT guid from {$dbprefix}entities e WHERE e.guid = " . $entity_guid;
-			// Add access controls
-			$query .= " AND " . $access_bit;
-			
-			if (get_data($query)) {
-				tag_tools_notify_user($user->guid, $entity->guid, $tag);
-			}
-		}		
+		$query = "SELECT guid FROM {$dbprefix}entities e
+			 WHERE e.guid = {$entity_guid}
+			 AND {$access_bit}";
+		
+		if (get_data($query)) {
+			// regsiter shutdown function because we need the full entity
+			// this is a workaround and should be reviewed in the near future
+			register_shutdown_function("tag_tools_notify_user", $user->getGUID(), $entity_row->guid, $tag);
+		}
 	}
 	
 	elgg_set_ignore_access($ia);
--- lib/functions.php
+++ lib/functions.php
@@ -3,15 +3,14 @@
  * All plugin functions are bundled here
  */
 
-function tag_tools_is_user_following_tag($tag, $user_guid = null, $reset_cache = false) {
+function tag_tools_is_user_following_tag($tag, $user_guid = 0, $reset_cache = false) {
 	static $follow_cache = array();
-	
-	$result = false;
 	
 	if (empty($tag)) {
 		return false;
 	}
 
+	$user_guid = sanitise_int($user_guid, false);
 	if (empty($user_guid)) {
 		$user_guid = elgg_get_logged_in_user_guid();
 	}
@@ -21,82 +20,101 @@ function tag_tools_is_user_following_tag($tag, $user_guid = null, $reset_cache =
 	}
 	
 	$user = get_user($user_guid);
-
-	if ($user) {
-		$ia = elgg_set_ignore_access(true);
-
-		$options = array(
-			'guid' => $user_guid,
-			'annotation_name' => "follow_tag",
-			'limit' => false
-		);
-		
-		$annotations = elgg_get_annotations($options);
-		
-		$follow_cache[$user_guid] = array();
-		foreach ($annotations as $annotation) {
-			$follow_cache[$user_guid][] = $annotation->value;
-		}
-		elgg_set_ignore_access($ia);
-		
-		return in_array($tag, $follow_cache[$user_guid]);
+	if (empty($user)) {
+		return false;
 	}
-
-	return $result;
+	
+	$options = array(
+		"guid" => $user_guid,
+		"annotation_name" => "follow_tag",
+		"limit" => false
+	);
+	
+	$ia = elgg_set_ignore_access(true);
+	$annotations = elgg_get_annotations($options);
+	elgg_set_ignore_access($ia);
+	
+	$follow_cache[$user_guid] = array();
+	foreach ($annotations as $annotation) {
+		$follow_cache[$user_guid][] = $annotation->value;
+	}
+	
+	return in_array($tag, $follow_cache[$user_guid]);
 }
 
-function tag_tools_toggle_following_tag($tag, $user_guid = null, $track = null) {
+function tag_tools_toggle_following_tag($tag, $user_guid = 0, $track = null) {
+	
+	$user_guid = sanitise_int($user_guid, false);
 	if (empty($user_guid)) {
 		$user_guid = elgg_get_logged_in_user_guid();
 	}
 
 	$user = get_user($user_guid);
+	if (empty($user)) {
+		return;
+	}
+	
+	if ($track === null) {
+		$track = !tag_tools_is_user_following_tag($tag, $user_guid);
+	}
 
-	if ($user) {
-		if ($track === null) {
-			$track = !tag_tools_is_user_following_tag($tag, $user_guid);
-		}
+	$options = array(
+		"guid" => $user_guid,
+		"limit" => false,
+		"annotation_name_value_pairs" => array(
+		 	"name" => "follow_tag",
+			"value" => $tag
+		)
+	);
 
-		$ia = elgg_set_ignore_access(true);
+	$ia = elgg_set_ignore_access(true);
+	elgg_delete_annotations($options);
+	elgg_set_ignore_access($ia);
 
-		$options = array(
-				'guid' => $user_guid,
-				'limit' => 0,
-				'annotation_name' => "follow_tag",
-				'annotation_value' => $tag
-		);
-
-		elgg_delete_annotations($options);
-		elgg_set_ignore_access($ia);
-
-		if ($track) {
-			$user->annotate("follow_tag", $tag);
-		}
+	if ($track) {
+		$user->annotate("follow_tag", $tag, ACCESS_PUBLIC);
 	}
 }
 
 function tag_tools_notify_user($user_guid, $entity_guid, $tag) {
-	static $notificiations;
+	static $notifications;
+	
+	$user_guid = sanitise_int($user_guid, false);
+	if (empty($user_guid)) {
+		return;
+	}
+	
+	$entity_guid = sanitise_int($entity_guid, false);
+	if (empty($entity_guid)) {
+		return;
+	}
 	
 	if (!isset($notifications)) {
 		$notifications = array();
 	}
 	
-	if (!array_key_exists($entity_guid, $notifications)) {
+	if (!isset($notifications[$entity_guid])) {
 		$notifications[$entity_guid] = array();
 	}
 	
-	if (!in_array($user_guid, $notifications[$entity_guid])) {
-		$activity_url = elgg_normalize_url("activity/tags");
-		system_message(elgg_echo("tag_tools:notification:follow:subject", array($tag)));
-		system_message($tag);
-		
-		$subject = elgg_echo("tag_tools:notification:follow:subject", array($tag));
-		$message = elgg_echo("tag_tools:notification:follow:message", array($tag, $activity_url));
-		
-		notify_user($user_guid, elgg_get_site_entity()->guid, $subject, $message, array());
-		
-		$notifications[$entity_guid][] = $user_guid;
+	if (in_array($user_guid, $notifications[$entity_guid])) {
+		// the user already received a notification about this entity, don't do it double
+		return;
 	}
 	
+	$ia = elgg_set_ignore_access(true);
+	$entity = get_entity($entity_guid);
+	elgg_set_ignore_access($ia);
+	
+	$subject = elgg_echo("tag_tools:notification:follow:subject", array($tag));
+	$message = elgg_echo("tag_tools:notification:follow:message", array($tag, $entity->getURL()));
+	
+	$params = array(
+		"action" => "tag_tools",
+		"object" => $entity
+	);
+	
+	notify_user($user_guid, elgg_get_site_entity()->getGUID(), $subject, $message, $params);
+	
+	$notifications[$entity_guid][] = $user_guid;
 }
