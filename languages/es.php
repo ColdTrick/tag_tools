c4afc4c061d6c6426516f6768d59c6e5ff462cf1
Yaco <franco@reevo.org>
2016-02-12 16:12:56 -0300

    add spanish translation

---
+++ languages/es.php
@@ -0,0 +1,21 @@
+<?php
+return [
+	'tag_tools:follow_tag:menu:on' => "Notificarme de los nuevos contenidos marcados con esta etiqueta",
+	'tag_tools:follow_tag:menu:off' => "Dejar de notificarme de los contenidos marcados con esta etiqueta",
+	'tag_tools:js:autocomplete:required' => "Por favor ingresa al menos una etiqueta",
+	'tag_tools:notifications:menu' => "Notificaciones de etiquetas",
+	'tag_tools:notifications:description' => "Configura las etiquetas sobre las cuales quieres recibir noficaciones  y de que forma quieres recibirlas.",
+	'tag_tools:notifications:empty' => "Actualmente no recibes notificaciones de ninguna etiqueta. Si ves una etiqueta sobre la cual quieres recibir notificaciones, clickea en 
+el icono al lado de ella.",
+	'tag_tools:notification:follow:subject' => "Hay un nuevo contenido con la etiqueta: %s",
+	'tag_tools:notification:follow:message' => "Hola,
+
+hay un nuevo contenido etiquetado con %s. Puedes verlo en este enlace: %s",
+	'tag_tools:activity:tags' => "Actividad relacionada a tus etiquetas",
+	'tag_tools:widgets:follow_tags:title' => "Notificiones de etiquetas",
+	'tag_tools:widgets:follow_tags:description' => "Muestra las etiquetas sobre las cuales se notifica",
+	'tag_tools:widgets:follow_tags:empty' => "Este usuario no recibe notificaciones de etiquetas",
+	'tag_tools:actions:follow_tag:success:follow' => "A partir de ahora serás notificado cuando alguien etiquete un contenido con la etiqueta <b>'%s'</b>",
+	'tag_tools:actions:follow_tag:success:unfollow' => "Ya no serás notificado cuando alguien etiquete un contenido con la etiqueta <b>'%s'</b>",
+];
+
