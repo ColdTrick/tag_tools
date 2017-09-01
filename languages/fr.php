14c3d65a8376a84728649cef512a503b4fe06d6e
fxn <fxn@afnor.org>
2017-09-01 11:44:04 +0200

    Add French translation

---
+++ languages/fr.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file was created by Translation Editor v5.2
+ * On 2017-09-01 11:11
+ */
+
+return array (
+  'tag_tools:notifications:description' => 'Configurer les centres d’intérêt que vous souhaitez suivre, comment et pour quel type de contenu vous serez notifié sur ces centres d’intérêt.',
+  'tag_tools:notifications:menu' => 'Notification de mes centres d’intérêt',
+  'tag_tools:actions:follow_tag:success:follow' => 'Vous suivez maintenant le tag : %s',
+  'tag_tools:actions:follow_tag:success:unfollow' => 'Vous ne suivez plus le tag : %s',
+  'tag_tools:notifications:empty' => 'Vous ne suivez aucun centre d’intérêt. Si vous voyez un centre d’intérêt que vous voulez suivre, cliquez sur le picto à coté de celui-ci.',
+  'tag_tools:widgets:follow_tags:title' => 'Tags suivis',
+  'tag_tools:widgets:follow_tags:description' => 'Indique les tags que vous suivez',
+  'tag_tools:notification:follow:update:message' => 'Bonjour, 
+
+Une nouvelle publication à été publiée contenant le tag : %s.
+Vous pouvez la consulter ici : %s
+
+Bonne lecture et à bientôt sur Parcours Croisés',
+  'tag_tools:notification:follow:message' => 'Bonjour,
+
+Un nouveau contenu a été publié avec le(s) tag(s): %s.
+
+Vous pouvez le consulter ici : %s',
+  'tag_tools:notification:follow:subject' => 'Nouveau contenu avec le(s) tag(s): %s',
+  'tag_tools:notification:follow:summary' => 'Nouveau contenu avec le(s) tag(s): %s',
+  'tag_tools:follow_tag:menu:on' => 'M\'avertir de tout nouveau contenu portant ce tag',
+  'tag_tools:follow_tag:menu:off' => 'Ne plus m\'avertir de tout nouveau contenu portant ce tag',
+  'tag_tools:js:autocomplete:required' => 'Merci de renseigner les tags,',
+  'admin:upgrades:set_tag_notifications_sent' => 'Tag tools - marquer tous les tags comme envoyé',
+  'admin:tags' => 'Tags',
+  'admin:tags:search' => 'Chercher',
+  'admin:tags:suggest' => 'Suggérer',
+  'admin:tags:rules' => 'Règles',
+  'item:object:tag_tools_rule' => 'Régle de Tag Tool',
+  'tag_tools:admin:tags:suggest:results:title' => 'Suggestions',
+  'tag_tools:admin:tags:suggest:item' => 'Tag <b>%s</b> pourrait être corrigé automatiquement par:',
+  'tag_tools:rule:title:delete' => 'Supprimer le tag %s',
+  'tag_tools:rule:title:replace' => 'Remplacer le tag \'%s\' par \'%s\'',
+  'tag_tools:search:count' => 'Nombre',
+  'tag_tools:search:rules' => 'Règles',
+  'tag_tools:search:replace' => 'Remplacer',
+  'tag_tools:rules:add' => 'Créer une nouvelle règle de tag',
+  'tag_tools:rules:edit' => 'Éditer la règle de tag',
+  'tag_tools:rules:from_tag' => 'Depuis le tag',
+  'tag_tools:rules:to_tag' => 'Vers le tag',
+  'tag_tools:rules:tag_action' => 'Action sur le tag',
+  'tag_tools:rules:tag_action:replace' => 'Rempalcer',
+  'tag_tools:rules:tag_action:delete' => 'Supprimer',
+  'tag_tools:rules:save_execute' => 'Enregistrer & exécuter',
+  'tag_tools:rules:execute' => 'Executer',
+  'tag_tools:notification:follow:update:subject' => 'Contenu mise à jour avec le(s) tag(s): %s',
+  'tag_tools:notification:follow:update:summary' => 'Contenu mise à jour avec le(s) tag(s): %s',
+  'tag_tools:activity:tags' => 'Activité basée sur vos tags',
+  'tag_tools:widgets:follow_tags:empty' => 'Cet utilisateur ne suit plus actuellement ce tag',
+  'tag_tools:widgets:tagcloud:description' => 'Affiche un nuage de cloud basé sur tous le contenu du site, dans le groupe ou relatif à un utilisateur',
+  'tag_tools:widgets:tagcloud:no_data' => 'Aucune donnée disponible pour afficher le nuage de tag',
+  'tag_tools:admin:tags:suggest:info' => 'La liste ci-dessous affiche les suggestions pour les autocorrections',
+  'admin:upgrades:set_tag_notifications_sent:description' => 'Marquer tous les tags sur le contenus suivis comme envoyé. Utile dans le cas d\'une montée de version du plugin, ou d\'un nouveau plugin',
+);
