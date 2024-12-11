6a9813671a021fdd7bce00e4904d0dae5b8a187b
Jerôme Bakker <jeabakker@coldtrick.com>
2024-12-11 14:24:28 +0100

    chore: wrapping up v12.0.2

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -10,7 +10,7 @@ require_once(dirname(__FILE__) . '/lib/functions.php');
 
 return [
 	'plugin' => [
-		'version' => '12.0.1',
+		'version' => '12.0.2',
 		'dependencies' => [
 			'tagcloud' => [
 				'position' => 'after',
